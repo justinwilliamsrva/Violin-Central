c8a2721a29d212d4f65cf855534154ba5f0a6ac4
Alvin <alvinzcoxiv@gmail.com>
2020-09-02 17:53:11 -0400

    changed project name in package.json

--- db/violin_db.sql
+++ db/violin_db.sql
@@ -24,3 +24,10 @@ PRIMARY Key(id)
 
 );
 
+INSERT INTO Exercises (exercise, book_title, author_composer, primary_positions, secondary_positions, primary_bowing, secondary_bowing, key, difficulty, focus, type, image, link, book_description) VALUES (1, "42 Etudes for Violin", "Kreutzer, Rodolphe", "1","2,3,4", "legato", "null", "A Minor", "Medium", "Tone", "Etude", "null", "null", "null")
+INSERT INTO Exercises (exercise, book_title,author_composer, primary_positions, secondary_positions, primary_bowing, secondary_bowing, key, difficulty, focus, type, image, link, book_description) VALUES (2, "42 Etudes for Violin", "Kreutzer, Rodolphe", "1","2,3,4", "legato", "null", "C Major", "Medium", "Facility", "Etude", "null", "null", "null")
+INSERT INTO Exercises (exercise, book_title, author_composer, primary_positions, secondary_positions, primary_bowing, secondary_bowing, key, difficulty, focus, type, image, link, book_description) VALUES (3, "42 Etudes for Violin", "Kreutzer, Rodolphe", "1", "2,3,4,5,5+", "detache", "null", "C Major ", "Medium", "Facility", "Etude", "null", "null" ,"null")
+
+INSERT INTO Teachers (first_name, last_name, city, state, email) VALUES ("Justin", "Williams", "Richmond", "Virginia", "justin.williams2@me.com");
+INSERT INTO Teachers (first_name, last_name, city, state, email) VALUES ("Katie", "Woolridge", "Richmond", "Virginia", "katharine.wooldridge@gmail.com");
+
