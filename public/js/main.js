e6e74418e83027cdf72b794534c25887bac5a377
Alvin <alvinzcoxiv@gmail.com>
2020-09-09 22:08:51 -0400

    main.js has been written for getting exercises, working on adjusting route

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(() => {
+  //Javascript to handle search for exercises
+  $(function() {
+    $(".exesubmit").on("click", function(event) {
+      event.preventDefault();
+
+      const searchParams = {
+        mainPosition: $("#mainpos").val(),
+        secondPosition: $("#pos2").val(),
+        mainBowing: $("#mainbow").val(),
+        secondBowing: $("#bow2").val(),
+        key: $("#key").val(),
+        difficulty: $("#difficulty").val(),
+        focus: $("#focus").val(),
+        type: $("#type").val()
+      };
+
+      if (searchParams.secondPosition === "N/A") {
+        searchParams.secondPosition = null;
+      }
+      if (searchParams.secondBowing === "N/A") {
+        searchParams.secondBowing = null;
+      }
+      if (searchParams.focus === "N/A") {
+        searchParams.focus = null;
+      }
+
+      $.ajax("/api/exercises", {
+        type: "GET",
+        data: searchParams
+      }).then(function() {
+        location.reload();
+      });
+    });
+  });
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,78 +4,78 @@ const passport = require("../config/passport");
 const { Op } = require("sequelize");
 
 module.exports = function(app) {
-    // Using the passport.authenticate middleware with our local strategy.
-    // If the user has valid login credentials, send them to the members page.
-    // Otherwise the user will be sent an error
-    app.post("/api/login", passport.authenticate("local"), (req, res) => {
-        // Sending back a password, even a hashed password, isn't a good idea
-        res.json({
-            email: req.user.email,
-            id: req.user.id,
-        });
+  // Using the passport.authenticate middleware with our local strategy.
+  // If the user has valid login credentials, send them to the members page.
+  // Otherwise the user will be sent an error
+  app.post("/api/login", passport.authenticate("local"), (req, res) => {
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      id: req.user.id
     });
+  });
 
-    // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-    // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-    // otherwise send back an error
-    app.post("/api/signup", (req, res) => {
-        db.User.create({
-            email: req.body.email,
-            password: req.body.password,
-        })
-            .then(() => {
-                res.redirect(307, "/api/login");
-            })
-            .catch((err) => {
-                res.status(401).json(err);
-            });
+  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+  // otherwise send back an error
+  app.post("/api/signup", (req, res) => {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(() => {
+        res.redirect(307, "/api/login");
+      })
+      .catch((err) => {
+        res.status(401).json(err);
+      });
+  });
+
+  // Route for logging user out
+  app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", (req, res) => {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
+  });
+
+  app.get("/exercises", (req, res) => {
+    const { mainPosition } = req.body.searchParams.mainPosition;
+    const { otherPosition } = req.body.searchParams.secondPosition;
+    const { mainBowing } = req.body.searchParams.mainBowing;
+    const { otherBowing } = req.body.searchParams.secondBowing;
+    const { key } = req.body.searchParams.key;
+    const { focus } = req.body.searchParams.focus;
+    const { type } = req.body.searchParams.type;
+
+    db.Exercises.findAll({
+      where: {
+        [Op.and]: [
+          { primary_positions: { [Op.like]: "%" + mainPosition + "%" } },
+          { secondary_positions: { [Op.like]: "%" + otherPosition + "%" } },
+          { primary_bowing: { [Op.like]: "%" + mainBowing + "%" } },
+          { secondary_bowing: { [Op.like]: "%" + otherBowing + "%" } },
+          { musical_key: { [Op.like]: "%" + key + "%" } },
+          { focus: { [Op.like]: "%" + focus + "%" } },
+          { type: { [Op.like]: "%" + type + "%" } }
+        ]
+      }
+    }).then(function(exercises) {
+      // console.log(exercises);
+      res.render("exercise", { exercises });
     });
-
-    // Route for logging user out
-    app.get("/logout", (req, res) => {
-        req.logout();
-        res.redirect("/");
-    });
-
-    // Route for getting some data about our user to be used client side
-    app.get("/api/user_data", (req, res) => {
-        if (!req.user) {
-            // The user is not logged in, send back an empty object
-            res.json({});
-        } else {
-            // Otherwise send back the user's email and id
-            // Sending back a password, even a hashed password, isn't a good idea
-            res.json({
-                email: req.user.email,
-                id: req.user.id,
-            });
-        }
-    });
-
-    app.get("/exercises", (req, res) => {
-        let { mainPosition } = req.query;
-        let { otherPosition } = req.query;
-        let { mainBowing } = req.query;
-        let { otherBowing } = req.query;
-        let { key } = req.query;
-        let { focus } = req.query;
-        let { type } = req.query;
-
-        db.Exercises.findAll({
-            where: {
-                [Op.and]: [
-                    { primary_positions: { [Op.like]: "%" + mainPosition + "%" } },
-                    { secondary_positions: { [Op.like]: "%" + otherPosition + "%" } },
-                    { primary_bowing: { [Op.like]: "%" + mainBowing + "%" } },
-                    { secondary_bowing: { [Op.like]: "%" + otherBowing + "%" } },
-                    { musical_key: { [Op.like]: "%" + key + "%" } },
-                    { focus: { [Op.like]: "%" + focus + "%" } },
-                    { type: { [Op.like]: "%" + type + "%" } },
-                ],
-            },
-        }).then(function(exercises) {
-            // console.log(exercises);
-            res.render("lessons", { exercises });
-        });
-    });
+  });
 };
