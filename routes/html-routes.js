b1a1de3c30d617b43d48953ae66d2ed44c60a781
justinwilliamsrva <justin.williams2@me.com>
2020-09-14 19:53:04 -0400

    Violin copy

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,34 +3,39 @@ const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
+const db = require("../models");
+const Sequelize = require("sequelize");
 // const Op = Sequelize.Op;
+const { Op } = require("sequelize");
 
 module.exports = function(app) {
-  app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
+    app.get("/", (req, res) => {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            res.redirect("/members");
+        }
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
 
-  app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+    app.get("/login", (req, res) => {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            res.redirect("/members");
+        }
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+    });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
+    // Here we've add our isAuthenticated middleware to this route.
+    // If a user who is not logged in tries to access this route they will be redirected to the signup page
+    app.get("/members", isAuthenticated, (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/members.html"));
+    });
 
-  app.get("/exercises", isAuthenticated, (req, res) => res.render("exercise"));
 
-  app.get("/exercises/add", (req, res) => res.render("post_exercise"));
+    app.get("/exercises/add", (req, res) => res.render("post_exercise"));
 
-  app.get("/lessons", isAuthenticated, (req, res) => res.render("search_lessons"));
+
+
+
+
 };
